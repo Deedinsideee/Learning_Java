91fdcfc69e1cd6137c2b62ded73cb300f53ab3bc
Deedinsideee <81362238+Deedinsideee@users.noreply.github.com>
2023-01-16 19:52:06 +0300

    DZ2P1N10

---
+++ src/HOME_WORK/DZ2/P1/N10.java
@@ -0,0 +1,36 @@
+package HOME_WORK.DZ2.P1;
+
+import java.util.Scanner;
+
+public class N10 {
+
+
+    public static void Game() {
+        int ran = (int) (Math.random() * 1000);
+        System.out.println(ran);
+        System.out.println("Ugadaite chislo!");
+        Scanner scanner = new Scanner(System.in);
+        int num ;
+        while (true) {
+            num = scanner.nextInt();
+            if (num == ran) {
+                System.out.println("True!!!");
+                break;
+            } else if (num > ran) {
+                System.out.println("Menshe!!!");
+            } else if (num < ran) {
+
+                System.out.println("Bolshe!!!");
+            }
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+            Game();
+    }
+
+
+}
